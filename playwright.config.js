de42411c7fa70af3e013b518457a4e6ec25567db
avinash-damale-123 <avinash.damale@satgurutravel.com>
2026-05-23 18:11:52 +0530

    Create playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,24 @@
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 60000,
+  expect: {
+    timeout: 10000
+  },
+  reporter: [
+    ['html'],
+    ['list']
+  ],
+  use: {
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] }
+    }
+  ]
+});
